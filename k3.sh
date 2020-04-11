29f8636b42cfa8c55558095c890381b7337e23b3
heham <heham@sohu.com>
2020-04-11 14:43:47 +0800

    Update k3.sh

--- k3.sh
+++ k3.sh
@@ -1,17 +1,25 @@
-wget -N --no-check-certificate https://github.com/heham/k3/raw/master/brcmfmac4366c-pcie.bin.69027
+cd /tmp
+opkg install libustream-openssl20150806
+wget --no-check-certificate https://github.com/heham/k3/raw/master/brcmfmac4366c-pcie.bin.69027
+wget --no-check-certificate https://github.com/heham/k3/raw/master/k3screenctrl.tar
+
 mv /lib/firmware/brcm/brcmfmac4366c-pcie.bin /lib/firmware/brcm/brcmfmac4366c-pcie.bin0
 cp ./brcmfmac4366c-pcie.bin.69027 /lib/firmware/brcm/brcmfmac4366c-pcie.bin
 chmod 755 /lib/firmware/brcm/brcmfmac4366c-pcie.bin
-rm /lib/firmware/brcm/brcmfmac4366c-pcie.bin0
-rm ./brcmfmac4366c-pcie.bin.69027
 
-wget -N --no-check-certificate https://github.com/heham/k3/raw/master/k3screenctrl.tar
-tar xvf ./k3screenctrl.tar
-cp cp -r ./k3screenctrl/ /lib/k3screenctrl/
-cp ./k3screenctrl/k3screenctrl /usr/bin/
+mkdir k3screenctrl
+tar xvf ./k3screenctrl.tar -C k3screenctrl
+cp -r ./k3screenctrl/lib/k3screenctrl/ /lib/k3screenctrl/
+cp ./k3screenctrl/bin/k3screenctrl /usr/bin/
+cp ./k3screenctrl/init.d/k3screenctrl /etc/init.d/
 chmod 755 /usr/bin/k3screenctrl
 chmod 755 /lib/k3screenctrl/*.*
-rm /lib/k3screenctrl/k3screenctrl
+chmod 755 /etc/init.d/k3screenctrl
+ln -s /etc/init.d/k3screenctrl /etc/rc.d/S40k3screenctrl
+
+rm /lib/firmware/brcm/brcmfmac4366c-pcie.bin0
+rm ./brcmfmac4366c-pcie.bin.69027
+rm ./k3screenctrl.tar
 rm -rf ./k3screenctrl/
 
 reboot
